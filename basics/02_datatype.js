63dd19ee4a2e9ac09ce221d116912cf1addc54bf
bhavanisaw <146553498+bhavanisaw@users.noreply.github.com>
2025-06-09 18:01:57 +0000

    datatype

---
+++ basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) we are using node jd not browser
+
+console.log(3+3);//code readability should be high
+ console.log("vani")
+
+ let name="vani"
+ let age = 18
+ let isloggedIn = false
+ let state;
+
+ //number =>2 to power 53
+ //bigint 
+ //string => ""
+ //boolean => true/false
+ //null =>standalone value
+ //undefined =>
+    //symbol =>unique
+
+
+ //object
+//null ek object ka datatype h aur undefined ka datatype undefined hi hai
+ console.log(typeof "vani");
